26d89b3120b4046c63f25507a199669a5f0b7b03
Malak-1990 <m.salamah@zu.edu.ly>
2025-06-23 10:23:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -22,10 +23,10 @@ export default defineConfig({
         lang: "ar",
         dir: "rtl",
         icons: [
-          { "src": "/icon-152x152.png", "sizes": "152x152", "type": "image/png", "purpose": "maskable any" },
-          { "src": "/icon-180x180.png", "sizes": "180x180", "type": "image/png", "purpose": "maskable any" },
-          { "src": "/icon-192x192.png", "sizes": "192x192", "type": "image/png", "purpose": "maskable any" },
-          { "src": "/icon-512x512.png", "sizes": "512x512", "type": "image/png", "purpose": "maskable any" }
+          { src: "/icon-152x152.png", sizes: "152x152", type: "image/png", purpose: "maskable any" },
+          { src: "/icon-180x180.png", sizes: "180x180", type: "image/png", purpose: "maskable any" },
+          { src: "/icon-192x192.png", sizes: "192x192", type: "image/png", purpose: "maskable any" },
+          { src: "/icon-512x512.png", sizes: "512x512", type: "image/png", purpose: "maskable any" }
         ],
         shortcuts: [
           {
@@ -33,22 +34,26 @@ export default defineConfig({
             short_name: "مريض",
             description: "مراقبة معدل ضربات القلب",
             url: "/patient-dashboard",
-            icons: [{ "src": "/icon-192x192.png", "sizes": "192x192" }]
+            icons: [{ src: "/icon-192x192.png", sizes: "192x192" }]
           },
           {
             name: "لوحة تحكم الطبيب",
             short_name: "طبيب",
             description: "متابعة المرضى",
             url: "/doctor-dashboard",
-            icons: [{ "src": "/icon-192x192.png", "sizes": "192x192" }]
+            icons: [{ src: "/icon-192x192.png", sizes: "192x192" }]
           }
         ]
       }
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   build: {
     sourcemap: true,
     target: "esnext"
   }
 } satisfies UserConfig);
-
